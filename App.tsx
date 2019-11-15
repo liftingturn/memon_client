16f01ec815106654f4d35c7d4581a8224f163b27
liftingturn <0oooceanhigh@gmail.com>
2019-11-15 13:14:15 +0900

    :wrench: android studio emulator done

--- App.tsx
+++ App.tsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import * as firebase from 'firebase';
-require('dotenv').config();
+// import * as firebase from 'firebase';
+// require('dotenv').config();
 
-const firebaseConfig = process.env.fbconfig;
+// const firebaseConfig = process.env.fbconfig;
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
+
+// try {
+//   firebase.auth().onAuthStateChanged(user => {
+//     if (user != null) {
+//       console.log('We are authenticated now!');
+//     } else {
+//       console.log('auth fail');
+//     }
+
+//     // Do other things
+//   });
+// } catch (err) {
+//   console.log('err:', err);
+// }
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>by joon</Text>
     </View>
   );
 }
