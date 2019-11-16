1a513a8f23536f14d35199919e61a880a47923bb
liftingturn <0oooceanhigh@gmail.com>
2019-11-16 18:19:28 +0900

    :construction: async componentdidmount in dashboard

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Component } from 'react';
 import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import axios from 'axios';
 
 interface Iprops {}
 
@@ -18,17 +19,30 @@ class DashboardScreen extends React.Component<Iprops, State> {
     };
   }
 
-  componentDidMount() {
-    this.setState({});
-  }
+  // async componentDidMount() {
+  //   try{
+  //    const newState =  await axios
+  //    .get('something url')
+  //    .then(res =>
+  //      {
+  //        moneyToPay: res.data.moneyToPay,
+  //        moneyToGet: res.data.moneyToGet
+  //      }
+  //    )
+  //   }catch(err) {
+  //     console.log(err)
+  //   }
+
+  //   this.setState(newState);
+  // }
 
   render() {
     return (
       <View style={styles.container}>
         <ActivityIndicator size="large" />
         <Text>
-          줄돈 ${this.state.moneyToPay}
-          <br></br>받을돈{this.state.moneyToGet}
+          줄돈 {this.state.moneyToPay}
+          받을돈{this.state.moneyToGet}
         </Text>
       </View>
     );
