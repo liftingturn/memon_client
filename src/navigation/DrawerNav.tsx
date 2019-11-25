25d1fc1f9090bebd9d2f81ef476b99295248dcf3
liftingturn <0oooceanhigh@gmail.com>
2019-11-25 16:44:23 +0900

    :wrench: delete unused import at drawerNav

--- src/navigation/DrawerNav.tsx
+++ src/navigation/DrawerNav.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import { Platform, Dimensions } from 'react-native';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import DashboardScreen from '../screens/DashboardScreen';
 import NewPayment from '../screens/NewPayment';
 import PaymentList from '../screens/PaymentList';
 import Profile from '../screens/Profile';
-import { Button } from 'native-base';
 import FriendScreen from '../screens/FriendScreen';
-import SingleView from '../screens/SingleView';
 import SingleViewPart from '../screens/SingleViewPart';
 
 const DrawerNav = createDrawerNavigator({
