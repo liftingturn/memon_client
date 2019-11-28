48fd4cf4a238a2ae5ae29cd516f954a080db9ca9
liftingturn <0oooceanhigh@gmail.com>
2019-11-28 19:45:49 +0900

    :bug: change server address

--- config.ts
+++ config.ts
@@ -9,7 +9,7 @@ const config = {
   measurementId: 'G-3BF7J6G9NZ',
   androidClientId:
     '209216463831-htvf7vv2lhmo4farouqkpg0ie46lc633.apps.googleusercontent.com',
-  serverAddress: 'http://db6db4f0.ngrok.io'
+  serverAddress: 'http://52.14.71.34:5000'
 };
 
 export default config;
