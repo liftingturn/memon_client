d34a55ee3759110f5cab6fad0f5c4ba0e04f5076
최방실 <qkdtlf89089@gmail.com>
2019-12-02 14:44:17 +0900

    :art: customize header font

--- src/components/DrawerHeader.tsx
+++ src/components/DrawerHeader.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { Header, Left, Button, Icon, Body, Right } from 'native-base';
+import { Header, Left, Button, Icon, Body, Right, Title } from 'native-base';
 import { screenStyles } from '../screenStyles';
 
 const DrawerHeader = props => {
   return (
-    <Header style={screenStyles.header}>
+    <Header style={{ ...screenStyles.header }}>
       <Left style={{ flex: 1 }}>
         <Button transparent onPress={props.toggleDrawer}>
           <Icon name="menu" />
         </Button>
       </Left>
-      <Body style={{ flex: 2, alignItems: 'center' }}>
-        <Text style={screenStyles.headerBodyText}>{props.title}</Text>
-      </Body>
+      <Title
+        style={{
+          paddingTop: 22,
+          alignItems: 'center',
+          fontFamily: 'GodoB',
+          fontSize: 25
+        }}
+      >
+        {/* <Text style={{ ...screenStyles.headerBodyText, fontFamily: 'GodoB' }}> */}
+        {props.title}
+        {/* </Text> */}
+      </Title>
       <Right style={{ flex: 1 }} />
     </Header>
   );
